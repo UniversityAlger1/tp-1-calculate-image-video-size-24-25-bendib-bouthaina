9df6d757a4c2020ebb6ee535cc2d206bb545036a
bendib-bouthaina <154970265+bendib-bouthaina@users.noreply.github.com>
2024-11-22 20:21:09 +0100

    Update video.c

--- video.c
+++ video.c
@@ -1,24 +1,27 @@
 #include <string.h>
 #include <stdio.h>
 #include "config/video.h"
+
 // Parameters:
-//   w: width of the image
-//   h: height of the image
-//   durationMovie: duration in second of movie (colored image)
-//   durationCredits: duration in second of credit (image Black/White)
-//   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
+//   w: width of the image
+//   h: height of the imag
+//   durationMovie: duration in seconds of the movie (colored image)
+//   durationCredits: duration in seconds of credits (black/white image)
+//   unit: Unit of the output value. It could be 'bt' for bytes, 'ko' for kilobits, 'mo' for megabits, 'go' for gigabits
 // Return value
-//   colored video size (based on the unit passed parametter)
+//   colored video size (based on the unit passed parameter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-   // YOUR CODE HERE - BEGIN
-// Bits per pixel for colored image (24 bits)
+    // Bits per pixel for colored image (24 bits)
     float bitsPerPixel = 24;
 
     float clrImage = w * h * bitsPerPixel * durationMovie * fps;
     float BImage = w * h * durationCredits * fps;
     float sizeInBits = clrImage + BImage; // Add to total size in bits
     float size;
-// Convert size based on the requested unit
+
+
+
+    // Convert size based on the requested unit
     if (strcmp(unit, "bt") == 0) {
         size = sizeInBits; // Convert to bytes
     } else if (strcmp(unit, "ko") == 0) {

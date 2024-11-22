c49e3c54bf3dc41e1df34e662f7e64c683aadda9
bendib-bouthaina <154970265+bendib-bouthaina@users.noreply.github.com>
2024-11-22 20:18:30 +0100

    Update video.c

--- video.c
+++ video.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include "config/video.h"
 // Parameters:
 //   w: width of the image
@@ -10,7 +11,27 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+// Bits per pixel for colored image (24 bits)
+    float bitsPerPixel = 24;
 
-   // YOUR CODE HERE - END
-   return 0;
+    float clrImage = w * h * bitsPerPixel * durationMovie * fps;
+    float BImage = w * h * durationCredits * fps;
+    float sizeInBits = clrImage + BImage; // Add to total size in bits
+    float size;
+// Convert size based on the requested unit
+    if (strcmp(unit, "bt") == 0) {
+        size = sizeInBits; // Convert to bytes
+    } else if (strcmp(unit, "ko") == 0) {
+        size = sizeInBits / (1024); // Convert to kilobits
+    } else if (strcmp(unit, "mo") == 0) {
+        size = sizeInBits / (1024 * 1024); // Convert to megabits
+    } else if (strcmp(unit, "go") == 0) {
+        size = sizeInBits / (1024 * 1024 * 1024); // Convert to gigabits
+    } else {
+        // If the unit is not recognized, return -1 or some error value
+        return -1.0f;
+    }
+
+    return size / 8;
 }
+   
